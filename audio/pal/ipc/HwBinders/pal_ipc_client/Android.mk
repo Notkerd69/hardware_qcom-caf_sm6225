da9b68adc78859ba84ebc008306b6961c33cb409
SGCMarkus <markusornik@gmail.com>
2022-11-06 14:28:15 +0100

    audio: pal: pal_ipc_client: dont error on unused parameters, variables and format
    Change-Id: I97e341b1bc929b1e9bdb3aa46df1aca419225fd5

--- audio/pal/ipc/HwBinders/pal_ipc_client/Android.mk
+++ audio/pal/ipc/HwBinders/pal_ipc_client/Android.mk
@@ -6,6 +6,9 @@ LOCAL_C_INCLUDES += $(call project-path-for,qcom-audio)/pal
 LOCAL_MODULE := libpalclient
 LOCAL_MODULE_OWNER := qti
 LOCAL_VENDOR_MODULE := true
+LOCAL_CFLAGS += -Wno-unused-parameter
+LOCAL_CFLAGS += -Wno-unused-variable
+LOCAL_CFLAGS += -Wno-format
 LOCAL_SRC_FILES := \
     src/pal_client_wrapper.cpp
 
